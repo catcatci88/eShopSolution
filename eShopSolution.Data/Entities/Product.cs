6e560f7bd8fff2bfe810b40aa3e8f3256cea10b5
Chiến Nguyễn <chiennx72@wru.vn>
2021-01-07 14:37:31 +0700

    update column database

--- eShopSolution.Data/Entities/Product.cs
+++ eShopSolution.Data/Entities/Product.cs
@@ -15,6 +15,8 @@ namespace eShopSolution.Data.Entities
         public string SeoAlias { get; set; }
 
         public List<ProductInCategory> ProductInCategories { get; set; }
+
+        public List<Cart> Carts { get; set; }
         public List<OrderDetail> OrderDetails { get; set; }
         public List<ProductTranslation> ProductTranslations { get; set; }
     }
--- eShopSolution.Data/Migrations/20210107045819_DataNew.Designer.cs
+++ eShopSolution.Data/Migrations/20210107045819_DataNew.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eShopSolution.Data.Entities_Framework;
 
 namespace eShopSolution.Data.Migrations
 {
     [DbContext(typeof(EShopDbContext))]
-    partial class EShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210107045819_DataNew")]
+    partial class DataNew
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -386,7 +388,7 @@ namespace eShopSolution.Data.Migrations
                     b.Property<DateTime>("OrderDate")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("datetime2")
-                        .HasDefaultValue(new DateTime(2021, 1, 7, 14, 36, 3, 340, DateTimeKind.Local).AddTicks(4300));
+                        .HasDefaultValue(new DateTime(2021, 1, 7, 11, 58, 19, 61, DateTimeKind.Local).AddTicks(8989));
 
                     b.Property<int?>("OrderDetailOrderId")
                         .HasColumnType("int");
