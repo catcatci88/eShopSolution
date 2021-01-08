a47fec6499300c3ceeab82254041db1a4ca5d1bb
Chiến Nguyễn <chiennx72@wru.vn>
2021-01-08 10:46:49 +0700

    update table productimage, productimageconfiguration

--- eShopSolution.Data/Entities/Cart.cs
+++ eShopSolution.Data/Entities/Cart.cs
@@ -13,5 +13,7 @@ namespace eShopSolution.Data.Entities
         public Guid UserId { get; set; }
         public Product Product { get; set; }
         public DateTime DateCreated { get; set; }
+
+        public List<Cart> Carts { get; set; }
     }
 }
